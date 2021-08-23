5ee36a0e4cc6e7b731590d6607c0f9abcee0f30b
Paul Bowden <paulbowden673@gmail.com>
2021-08-23 19:24:41 +0000

    added JS fucntion to remove hoer style on touch devices to fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -197,6 +197,38 @@ let score = 0;
 const SCORE_POINTS = 10;
 const MAX_QUESTIONS = 20;
 
+
+// remove hover on touch devices
+function watchForHover() {
+    // lastTouchTime is used for ignoring emulated mousemove events
+    // that are fired after touchstart events. Since they're
+    // indistinguishable from real events, we use the fact that they're
+    // fired a few milliseconds after touchstart to filter them.
+    let lastTouchTime = 0
+  
+    function enableHover() {
+      if (new Date() - lastTouchTime < 500) return
+      document.body.classList.add('hasHover')
+    }
+  
+    function disableHover() {
+      document.body.classList.remove('hasHover')
+    }
+  
+    function updateLastTouchTime() {
+      lastTouchTime = new Date()
+    }
+  
+    document.addEventListener('touchstart', updateLastTouchTime, true)
+    document.addEventListener('touchstart', disableHover, true)
+    document.addEventListener('mousemove', enableHover, true)
+  
+    enableHover()
+  }
+  
+  watchForHover()
+
+
 //function for new question
 
 function renderQuestion() {
